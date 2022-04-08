8f422f6d9cd54f8d68991ec9c5a3d578bb1cab0e
Ivan <94782858+ivan90s@users.noreply.github.com>
2022-04-08 18:00:45 +0200

    states for candidates and reports

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import Candidates from "../Candidates/Candidates";
 import Interviews from "../Interviews/Interviews";
 import SingleCandidate from "../SingleCandidate/SingleCandidate";
@@ -7,6 +7,25 @@ import { Switch, Route } from "react-router-dom";
 import "./style.scss";
 
 const HomePage = () => {
+
+    const [candidates,setCandidates] = useState([]);
+    const [interviews,setInterviews] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:3333/api/candidates')
+        .then(res=>res.json())
+        .then(data=>setCandidates(data))
+    },[])
+
+    useEffect(()=>{
+        fetch('http://localhost:3333/api/reports')
+        .then(res=>res.json())
+        .then(data=>setInterviews(data))
+    },[])
+
+    console.log(candidates);
+    console.log(interviews);
+
     return ( 
         <div className="homePage">
             <h1>Hello from HomePage</h1>
