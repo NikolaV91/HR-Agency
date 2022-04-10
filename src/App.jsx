f4167e57471333c19e0370fbcb60f6443dd0101e
nesawesly <nesawesly@gmail.com>
2022-04-10 22:26:14 +0200

    login

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,27 @@ import React from "react";
 import Login from "./pages/Login/Login";
 import HomePage from "./pages/HomePage/HomePage"
 import { Switch, Route } from "react-router-dom";
+import { useState,} from "react";
+
+
+
+
 
 const App = () => {
 
+  const tokenHolder = React.createContext(null)
+  const Provider = tokenHolder.Provider
 
+  
+  const [tokenFromStorage,getTokenFromStorage] = useState(localStorage.getItem("token"))
+
+  console.log(tokenFromStorage)
+  
 
   return (
     <div className="app">
+      
+      <Provider value={{tokenFromStorage,getTokenFromStorage}}>
       <Switch>
         <Route path="/homepage">
           <HomePage />
@@ -18,7 +32,9 @@ const App = () => {
           <Login />
         </Route>
       </Switch>
-    </div>
+      </Provider>
+   
+  </div>
   );
 }
 
--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.scss"
 import {useState} from "react"
+import HomePage from "../HomePage/HomePage"
 
 const Login = (props) => {
 
@@ -8,36 +9,72 @@ const Login = (props) => {
 
 const [email,setEmail] = useState("")
 const [password, setPassword] = useState("")
+// const [token, setToken]= useState(localStorage.getItem("token"))
+const [enter ,setEntry] = useState(false)
 
 
-const setUsername = (input) => {
-    setEmail(input)
+
+
+const setUsername = (event) => {
+    setEmail(event)}
+
+const setPass = (event) => {
+    setPassword(event)
 }
 
-const setPass = (input) => {
-    setPassword(input)
+
+const logovanje = () => {
+    fetch('http://localhost:3333/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
+    }).then(res => res.json()).then(res=> {if(typeof res === "string"){
+        alert("Niste dobro uneli korisnicko ime ili sifru")
+        
+    }
+    else if(typeof res === "object"){
+        setEntry(true)
+        localStorage.setItem("token", res.accessToken)
+        // setToken(localStorage.getItem("token"))
+         if (localStorage.getItem("token")){
+            alert("Uspesno ste se ulogovali")
+    } 
+    
+    }
+
+    } )
+   
+        
 }
 
 
+// console.log(token)
 
 
 
-console.log(email);
-console.log(password)
 
 
 return ( 
+    enter ? <HomePage/> :
     <div className="login">
+       
             <h1>Hello from Logins</h1> 
             <label>
-                Username:
+                E-mail:
                 <input type="email" placeholder="username" onChange={(e)=> setUsername(e.target.value)}/>
             </label>
             <label >
                 Password:
             <input type="password" placeholder="password" onChange={(e)=> setPass(e.target.value)}/>
             </label>
+            <button onClick={logovanje}>Login</button>
         </div>
+        
      );
 }
  
