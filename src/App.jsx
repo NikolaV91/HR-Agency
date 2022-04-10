e756c01d472eef197c9791d4bf055df7992bd7b6
nesawesly <nesawesly@gmail.com>
2022-04-10 22:26:14 +0200

    login

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Login from "./pages/Login/Login";
 import HomePage from "./pages/HomePage/HomePage";
 import { Switch, Route } from "react-router-dom";
+import { useState } from "react";
 
 import { TokenProvider } from "../../contexts/contexts";
 
@@ -10,17 +11,19 @@ const App = () => {
 
   return (
     <div className="app">
-      <Switch>
-        <TokenProvider value={{ token, setToken }}>
-          <Route path="/homepage">
-            <HomePage />
-          </Route>
+      <Provider value={{ tokenFromStorage, getTokenFromStorage }}>
+        <Switch>
+          <TokenProvider value={{ token, setToken }}>
+            <Route path="/homepage">
+              <HomePage />
+            </Route>
 
-          <Route path="/">
-            <Login />
-          </Route>
-        </TokenProvider>
-      </Switch>
+            <Route path="/">
+              <Login />
+            </Route>
+          </TokenProvider>
+        </Switch>
+      </Provider>
     </div>
   );
 };
--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,44 +1,74 @@
 import React from "react";
-import "./style.scss"
-import {useState} from "react"
+import "./style.scss";
+import { useState } from "react";
+import HomePage from "../HomePage/HomePage";
 
 const Login = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const [token, setToken]= useState(localStorage.getItem("token"))
+  const [enter, setEntry] = useState(false);
 
+  const setUsername = (event) => {
+    setEmail(event);
+  };
 
+  const setPass = (event) => {
+    setPassword(event);
+  };
 
-const [email,setEmail] = useState("")
-const [password, setPassword] = useState("")
+  const logovanje = () => {
+    fetch("http://localhost:3333/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (typeof res === "string") {
+          alert("Niste dobro uneli korisnicko ime ili sifru");
+        } else if (typeof res === "object") {
+          setEntry(true);
+          localStorage.setItem("token", res.accessToken);
+          // setToken(localStorage.getItem("token"))
+          if (localStorage.getItem("token")) {
+            alert("Uspesno ste se ulogovali");
+          }
+        }
+      });
+  };
 
+  // console.log(token)
 
-const setUsername = (input) => {
-    setEmail(input)
-}
-
-const setPass = (input) => {
-    setPassword(input)
-}
-
-
-
-
-
-console.log(email);
-console.log(password)
-
-
-return ( 
+  return enter ? (
+    <HomePage />
+  ) : (
     <div className="login">
-            <h1>Hello from Login2</h1> 
-            <label>
-                Username:
-                <input type="email" placeholder="username" onChange={(e)=> setUsername(e.target.value)}/>
-            </label>
-            <label >
-                Password:
-            <input type="password" placeholder="password" onChange={(e)=> setPass(e.target.value)}/>
-            </label>
-        </div>
-     );
-}
- 
+      <h1>Hello from Logins</h1>
+      <label>
+        E-mail:
+        <input
+          type="email"
+          placeholder="username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPass(e.target.value)}
+        />
+      </label>
+      <button onClick={logovanje}>Login</button>
+    </div>
+  );
+};
+
 export default Login;
