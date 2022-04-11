b1f029dba4d24a694814c3126ae104f9ba3ebd74
Ivan <94782858+ivan90s@users.noreply.github.com>
2022-04-11 11:18:20 +0200

    context

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import Login from "./pages/Login/Login";
-import HomePage from "./pages/HomePage/HomePage"
+import HomePage from "./pages/HomePage/HomePage";
 import { Switch, Route } from "react-router-dom";
 
+import { TokenProvider } from "../../contexts/contexts";
+
 const App = () => {
-
-
+  const [token, setToken] = useState("");
 
   return (
     <div className="app">
       <Switch>
-        <Route path="/homepage">
-          <HomePage />
-        </Route>
-        
-        <Route path="/">
-          <Login />
-        </Route>
+        <TokenProvider value={{ token, setToken }}>
+          <Route path="/homepage">
+            <HomePage />
+          </Route>
+
+          <Route path="/">
+            <Login />
+          </Route>
+        </TokenProvider>
       </Switch>
     </div>
   );
-}
+};
 
 export default App;
