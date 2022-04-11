a2792ea2eba3a93c2168aec5538957a6422c2014
Ivan <94782858+ivan90s@users.noreply.github.com>
2022-04-11 16:29:13 +0200

    single card

--- src/components/CandidateCard/CandidateCard.jsx
+++ src/components/CandidateCard/CandidateCard.jsx
@@ -7,13 +7,11 @@ import "./style.scss";
 const CandidateCard = (props) => {
     
     return (
-        <Link to="/homepage/singlecandidate">
             <div className="candidateCard">
                 <img src="https://static.vecteezy.com/system/resources/thumbnails/000/439/863/small/Basic_Ui__28186_29.jpg" alt="" />
-                <h1>{props.singleCandidate.name}</h1>
-                <h3>{props.singleCandidate.email}</h3>
+                <h1>{props.candidates.name}</h1>
+                <h3>{props.candidates.email}</h3>
             </div>
-        </Link>
      );
 }
  
--- src/pages/Candidates/Candidates.jsx
+++ src/pages/Candidates/Candidates.jsx
@@ -1,31 +1,34 @@
-import React, { useContext } from "react";
+import { React, useContext } from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import SearchCandidates from "../../components/SearchCandidates/SearchCandidates";
 import CandidateCard from "../../components/CandidateCard/CandidateCard";
-import {candidatesContext} from "../../contexts/contexts"
+import { Link } from "react-router-dom";
 
-import "./style.scss"
+import "./style.scss";
+import { candidatesContext } from "../../contexts/contexts";
 
 const Candidates = () => {
+  const { candidates } = useContext(candidatesContext);
 
-    const {candidates} = useContext(candidatesContext)
-    
-
-
-    return (
-        <div className="candidates">
-            <Header/>
-            <div className="candidatesWrapper">
-            <SearchCandidates/>
-                <div className="candidatesMain">
-                    {candidates.map((e)=>{
-                        return <CandidateCard singleCandidate={e} />})}
-                    </div>
-                </div>
-            <Footer/>
+  return (
+    <div className="candidates">
+      <Header />
+      <div className="candidatesWrapper">
+        <SearchCandidates />
+        <div className="candidatesMain">
+          {candidates.map((e) => {
+            return (
+              <Link key={e.id} to={`/homepage/candidates/singlecandidate/${e.id}`}>
+                <CandidateCard candidates={e} />
+              </Link>
+            );
+          })}
         </div>
-     );
-}
- 
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
 export default Candidates;
