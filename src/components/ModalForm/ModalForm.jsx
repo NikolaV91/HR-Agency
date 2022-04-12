70b36919bdef19cbfe33a522e6921589be187392
Ivan <94782858+ivan90s@users.noreply.github.com>
2022-04-12 17:07:07 +0200

    modal interview

--- src/components/ModalForm/ModalForm.jsx
+++ src/components/ModalForm/ModalForm.jsx
@@ -1,12 +1,27 @@
-import React from "react";
-import "./style.scss"
+import { React, useState } from "react";
+import "./style.scss";
 
-const ModalForm = () => {
-    return ( 
-        <div className="modalForm">
-            <h1>Hello from Modal Card</h1>
+const ModalInterview = (props) => {
+
+  return (
+    <div className="modalInterview">
+      <div className="modal-wrapper">
+        <div className="modal-content">
+          <button className="close-modal" onClick={(e)=>{
+              props.modalShouldUpdate()
+          }}> X </button>
+          <div>
+          <p>Candidate name: {props.interview.candidateName}</p>
+          <p>Company name: {props.interview.companyName}</p>
+          <p>Interview date: {props.interview.interviewDate}</p>
+          <p>Phase: {props.interview.phase}</p>
+          <p>Status: {props.interview.candidateName}</p>
+          </div>
+          <p>Note: {props.interview.note}</p>
         </div>
-     );
-}
- 
-export default ModalForm;
+      </div>
+    </div>
+  );
+};
+
+export default ModalInterview;
--- src/components/ModalInterview/ModalInterview.jsx
+++ src/components/ModalInterview/ModalInterview.jsx
@@ -1,12 +1,27 @@
-import React from "react";
-import "./style.scss"
+import { React, useState } from "react";
+import "./style.scss";
 
-const ModalInterview = () => {
-    return ( 
-        <div className="modalInterview">
-            <h1>Hello from Modal Interview</h1>
+const ModalInterview = (props) => {
+
+  return (
+    <div className="modalInterview">
+      <div className="modal-wrapper">
+        <div className="modal-content">
+          <button className="close-modal" onClick={(e)=>{
+              props.modalShouldUpdate()
+          }}> X </button>
+          <div>
+          <p>Candidate name: {props.interview.candidateName}</p>
+          <p>Company name: {props.interview.companyName}</p>
+          <p>Interview date: {props.interview.interviewDate}</p>
+          <p>Phase: {props.interview.phase}</p>
+          <p>Status: {props.interview.candidateName}</p>
+          </div>
+          <p>Note: {props.interview.note}</p>
         </div>
-     );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default ModalInterview;
