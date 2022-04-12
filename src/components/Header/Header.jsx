67dea95ce93b7aef03131d82d885103469cb544d
NikolaV91 <vasic.nikol@outlook.com>
2022-04-12 11:31:23 +0200

    updates on login functionality and style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,16 @@ import { Link } from "react-router-dom";
 import "./style.scss"
 
 const Header = () => {
+    const removeToken = ()=>{
+        localStorage.removeItem("token")
+    }
     return ( 
         <div className="header">
             <h1>HR Agency</h1>
             <div className="headerButton">
                 <Link to="/homepage/candidates"><button>Candidates</button></Link>
                 <Link to="/homepage/interviews"><button>Interviews</button></Link>
-                <Link to="/"><button className="logout">Log Out</button></Link> 
+                <Link to="/"><button className="logout" onClick={removeToken}>Log Out</button></Link> 
             </div>
         </div>
      );
