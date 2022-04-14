a3da5b81c40484cd233b89572076a3f4fa46a40f
NikolaV91 <vasic.nikol@outlook.com>
2022-04-14 18:04:54 +0200

    updates on singlepage design

--- src/pages/SingleCandidate/SingleCandidate.jsx
+++ src/pages/SingleCandidate/SingleCandidate.jsx
@@ -5,7 +5,11 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import ModalForm from "../../components/ModalForm/ModalForm";
 import ModalInterview from "../../components/ModalInterview/ModalInterview";
-import ModalUpdateForm from "../../components/ModalUpdateForm/ModalUpdateForm"
+import ModalUpdateForm from "../../components/ModalUpdateForm/ModalUpdateForm";
+// import garbageClosed from "../../images/garbageClosed.png";
+// import garbageOpen from "../../images/garbage-open.png"
+// import editIcon from "../../images/edit-icon.png";
+// import viewIcon from "../../images/view-icon.png";
 
 
 import "./style.scss";
@@ -64,47 +68,49 @@ const SingleCandidate = (props) => {
     console.log(singleCandidate)
     return (
       <div className="singleCandidate">
-        <Header />
+      <Header />
         <div className="singleCandidateContainer">
           <div className="data">
           <img src="https://static.vecteezy.com/system/resources/thumbnails/000/439/863/small/Basic_Ui__28186_29.jpg" alt="imageAvatar"></img>
             <div className="dataDetails">
-            <h3>Name: <br /> {singleCandidate.name}</h3>
-            <h3>Birthday: <br /> {singleCandidate.birthday}</h3>
+            <h3> <span>Name:</span>  <br />  {singleCandidate.name}</h3>
+            <h3> <span>Birthday:</span>  <br />  {singleCandidate.birthday.slice(4,16)}</h3>
             </div>
             <div className="dataDetails">
-            <h3>Education: <br /> {singleCandidate.education}</h3>
-            <h3>Email: <br /> {singleCandidate.email}</h3>
+            <h3> <span>Education:</span> <br />{singleCandidate.education}</h3>
+            <h3> <span>Email:</span> <br />{singleCandidate.email}</h3>
             </div>
           </div>
         </div>
-
+        <h1>Interviews</h1>
         <table>
           <tbody>
             <tr>
-              <th>Company name</th>
+              <th id="firstTH">Company name</th>
               <th>Interview date</th>
               <th>Phase</th>
               <th>Status</th>
               <th>View notes</th>
               <th>Edit interview</th>
-              <th>Delete Interview</th>
+              <th id="lastTH">Delete Interview</th>
             </tr>
             {singleCandidateReport.map((e) => (
               <>
-                <tr>
+                <tr className="onHover">
                   <td>{e.companyName}</td>
                   <td>{e.interviewDate}</td>
                   <td>{e.phase}</td>
                   <td>{e.status}</td>
                   <td>
-                    <button onClick={() => { modalShouldUpdate(e) }}>view</button>
+                    <button className="view" onClick={() => { modalShouldUpdate(e) }}>  </button>
                   </td>
                   <td>
-                    <button onClick={() => { formEditModalShouldUpdate(e) }}>edit</button>
+                    <button className="edit" onClick={() => { formEditModalShouldUpdate(e) }}> </button>
                   </td>
                   <td>
-                    <button onClick={() => deleteInterview(e)}>delete</button>
+                    <button className="garbage" onClick={() => deleteInterview(e)}>
+                     {/* <div ></div> */}
+                    </button>
                   </td>
                 </tr>
               </>
@@ -115,7 +121,7 @@ const SingleCandidate = (props) => {
         {interviewModal && <ModalInterview interview={interviewModal} modalShouldUpdate={modalShouldUpdate} />}
         {formModalUpdate && <ModalUpdateForm interview={formModalUpdate} formEditModalShouldUpdate={formEditModalShouldUpdate} setShouldUpdate={props.setShouldUpdate} />}
 
-        <button onClick={(e) => { formModalShouldUpdate() }}>CREATE INTERVIEW</button>
+        <button className="newInterviewBTN" onClick={(e) => { formModalShouldUpdate() }}>CREATE INTERVIEW</button>
         {formModal && <ModalForm formModalShouldUpdate={formModalShouldUpdate} setShouldUpdate={props.setShouldUpdate} />}
 
         <Footer />
