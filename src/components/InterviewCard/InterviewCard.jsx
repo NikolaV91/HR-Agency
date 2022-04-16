114fe1f03158a3c5cda236ee12703bce2b2169e6
NikolaV91 <vasic.nikol@outlook.com>
2022-04-16 20:28:04 +0200

    small update on interviewCard

--- src/components/InterviewCard/InterviewCard.jsx
+++ src/components/InterviewCard/InterviewCard.jsx
@@ -11,7 +11,7 @@ const InterviewCard = (props) => {
             <h2 className="names">{props.singleInterview.candidateName} <br /><span>Candidates</span></h2>
             <h2 className="dates">{props.singleInterview.interviewDate} <br /><span>Date of Interview</span></h2>
             <h2 className="phase">{props.singleInterview.phase} <br /><span>Phase</span></h2>
-            <h2 className="status">{props.singleInterview.status} <br /><span>Status</span></h2>
+            <h2 className={props.singleInterview.status === "passed" ? "status greenStatus" : "status redStatus"}>{props.singleInterview.status} <br /><span>Status</span></h2>
         </div>
     );
 }
